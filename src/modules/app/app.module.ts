9033005e2012da7bbde573c921c2867b3d85534f
Temirlan <temirlan.termechikov@gmail.com>
2023-04-22 15:39:08 +0600

    Configured a connection to a database

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from '../user/user.module';
+import { ormconfig } from 'src/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormconfig), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ormconfig.ts
@@ -0,0 +1,35 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+const {
+  POSTGRES_HOST: host,
+  POSTGRES_PORT: port,
+  POSTGRES_USER: username,
+  POSTGRES_PASSWORD: password,
+  POSTGRES_DB: database,
+} = process.env;
+
+export const ormconfig: DataSourceOptions = {
+  type: 'postgres',
+  host,
+  port: parseInt(port),
+  username,
+  password,
+  database,
+  synchronize: false,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/db/migrations/**/*{.ts,.js}'],
+};
+
+const AppDataSource = new DataSource(ormconfig);
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
+export default AppDataSource;
